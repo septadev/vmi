2fe5b0b9b7e5077dd1498f6ddb60bfda1a32ae20
Mike <mruberto@septa.org>
2014-02-28 15:55:06 -0500

    reording javascript import on result template.

--- static/src/js/results.js
+++ static/src/js/results.js
@@ -1,10 +1,19 @@
 /**
  * Created by M. A. Ruberto on 2/28/14.
  */
-$('#contents').dataTable( {
+$(document).ready(function(){
+  $('#contents').dataTable( {
     "aaData": history_data,
 	"sDom": 'T<"clear">lfrtip',
 	"oTableTools": {
 					"sSwfPath": "/vmi/static/src/js/datatables/extras/TableTools/media/swf/copy_csv_xls_pdf.swf"
-					}
+					},
+     "aoColumns":[
+         {"mData": "date"},
+         {"mData": "id"},
+         {"mData": "origin"},
+         {"mData": "state"},
+         {"mData": "purchase_id"}
+     ]
 } );
+});
