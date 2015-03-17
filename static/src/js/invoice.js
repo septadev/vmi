6d86fe275a292bf8c12c4016a1b71d2b35c5f5fa
Xiang Li <lx881219@gmail.com>
2015-03-17 10:17:55 -0400

    Add download feature on Invoice page and fix bug
    1. Allow vendor to download Invoice details in csv format.
    2. Fix jQuery selector on invoice page.

--- static/src/js/invoice.js
+++ static/src/js/invoice.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
             {
                 "mDataProp": null,
                 "sClass": "control center",
-                "sDefaultContent": '<img src="/vmi/static/src/img/details_open.png">'
+                "sDefaultContent": '<img id="expand" src="/vmi/static/src/img/details_open.png"><img id="save" src="/vmi/static/src/img/save.png">'
             },
             {"mData": "date_invoice"},
             {"mData": "name"},
@@ -57,8 +57,8 @@ $(document).ready(function() {
                             //'<input id="comment" name="comment" type="hidden">' +
                             //'<input type="image" name="approved" title="Approve" src="/vmi/static/src/img/gtk-yes.png" alt="Submit">' +
                             //'<input type="image" name="denied" title="Deny" src="/vmi/static/src/img/gtk-no.png" alt="Submit" style="margin-left: 30px">'
-                            '<button type="submit" class="approved" name="result" value="approved" title="Approve"><img title="Approve" src="/vmi/static/src/img/gtk-yes.png"></button>' +
-                            '<button type="submit" class="denied" name="result" value="denied" title="Deny"><img title="Deny" src="/vmi/static/src/img/gtk-no.png"></button>' +
+                            '<button type="submit" id="approved" name="result" value="approved" title="Approve"><img title="Approve" src="/vmi/static/src/img/gtk-yes.png"></button>' +
+                            '<button type="submit" id="denied" name="result" value="denied" title="Deny"><img title="Deny" src="/vmi/static/src/img/gtk-no.png"></button>' +
                             '</form>';
                         /* +
                          '<div id="leave_comment" title="Comment" type="hidden">' +
@@ -78,16 +78,14 @@ $(document).ready(function() {
         ],
         "sPaginationType": "full_numbers"
     });
-    $('#contents td.control').live('click', function () {
-        var nTr = this.parentNode;
+    $('#contents').on('click', '#expand', function () {
+        var nTr = this.parentNode.parentNode;
         var i = $.inArray(nTr, anOpen);
-
         if (i === -1) {
-            $('img', this).attr('src', "/vmi/static/src/img/details_close.png");
+            $(this).attr('src', "/vmi/static/src/img/details_close.png");
             var aData = oTable.fnGetData(nTr);
             var line_ids = aData.invoice_line;
             var uid = sessionStorage.getItem(('user_id'));
-            var rowDetails;
             $.ajax({
                 type: "POST",
                 url: "/vmi/get_invoice_lines",
@@ -115,10 +113,9 @@ $(document).ready(function() {
 
                 }
             });
-
         }
         else {
-            $('img', this).attr('src', "/vmi/static/src/img/details_open.png");
+            $(this).attr('src', "/vmi/static/src/img/details_open.png");
             $('div.innerDetails', $(nTr).next()[0]).slideUp(function () {
                 oTable.fnClose(nTr);
                 anOpen.splice(i, 1);
@@ -126,38 +123,42 @@ $(document).ready(function() {
         }
     });
 
-    /*$("form").submit(function(){
-     var message = $("#comment").val();
-     alert(message);
-     message = prompt("Please enter the comment regarding this invoice:");
-     if (message != null){
-     $("#comment").val(message);
-     }
-     alert($("#comment").val());
-     });*/
-    /*function getComment(){
-     message = $("#textarea").val();
-     if (message != null){
-     var app = '<input name="comment" value="' + message + '" type="hidden">';
-     $("form").append(app);
-     }
-     }
-     var dialog = "#leave_comment".dialog({
-     autoOpen: false,
-     height:300,
-     width: 350,
-     modal: true,
-     buttons:{
-     "Submit": getComment,
-     Cancel: function(){
-     dialog.dialog("close");
-     }
-     },
-     close: function(){
-     dialog.dialog("close");
-     }
-     });*/
-    $(".denied").click(function () {
+    $('#contents tbody').on('click', '#save', function() {
+        var nTr = this.parentNode.parentNode;
+        var i = $.inArray(nTr, anOpen);
+        var aData = oTable.fnGetData(nTr);
+        var line_ids = aData.invoice_line;
+        var uid = sessionStorage.getItem(('user_id'));
+        var rowDetails
+        $.ajax({
+            type: "POST",
+            url: "/vmi/get_invoice_lines",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: '{"jsonrpc": "2.0","method":"call","params":{"session_id": "' + sessionid + '", "context": {}, "ids": "' + line_ids + '", "uid": "' + uid + '"},"id":"VMI"}',
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+
+            },
+            success: function (data) {
+                if (data.result && data.result.error) { // script returned error
+                    $('div#loginResult').text("Warning: " + data.result.error);
+                    $('div#loginResult').addClass("notice");
+                }
+                else if (data.error) { // OpenERP error
+                    $('div#loginResult').text("Error-Message: " + data.error.message + " | Error-Code: " + data.error.code + " | Error-Type: " + data.error.data.type);
+                    $('div#loginResult').addClass("error");
+                } // if
+                else { // successful transaction
+                    rowDetails = data.result;
+                    var title = aData.name + "-" + aData.date_invoice;
+                    json2csv(rowDetails, title);
+                }
+            }
+        });
+
+    });
+
+    $('#contents tbody').on('click', '#denied', function () {
         $("form").submit(function (e) {
             //dialog.dialog("open");
 
@@ -181,7 +182,7 @@ $(document).ready(function() {
         });
         //$(this).attr("type", "submit");
     });
-    $(".approved").click(function () {
+    $('#contents tbody').on('click', '#approved', function () {
         $("form").submit(function () {
             var app = '<input name="comment" value="" type="hidden">';
             $("form").append(app);
@@ -217,5 +218,65 @@ $(document).ready(function() {
         return sOut;
     }
 
+    function json2csv(objArray, title) {
+        var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+        var res = '';
+        var line = '';
+        var fields = {"date_received": "Invoice Date", "picking_number": "Packing Number", "septa_part_number": "Septa P/N", "vendor_part_number": "Vendor P/N", "unit_of_measure": "Unit of Measure", "quantity": "Quantity", "price_unit": "Price Unit", "price_subtotal": "Price Subtotal"}
+        /*for (var index in array[0]) {
+            if ($.inArray(index, Object.keys(fields)) > -1){
+                var value = fields[index] + "";
+                line += '"' + value.replace(/"/g, '""') + '",';
+            }
+            //var value = index + "";
+            //line += '"' + value.replace(/"/g, '""') + '",';
+        }*/
+        for (var index in fields){
+            var value = fields[index] + "";
+            line += '"' + value.replace(/"/g, '""') + '",';
+        }
+        line = line.slice(0, -1);
+        res += line + '\r\n';
+        /*for (var i = 0; i < array.length; i++) {
+            var line = '';
+            for (var index in array[i]) {
+                if ($.inArray(index, Object.keys(fields)) > -1){
+                    if (array[i][index].constructor == Array){
+                        var value = array[i][index][1] + "";
+                    }
+                    else{
+                        var value = array[i][index] + "";
+                    }
+                    line += '"' + value.replace(/"/g, '""') + '",';
+                }
+            }
+            line = line.slice(0, -1);
+            res += line + '\r\n';
+        }*/
+        for (var i = 0; i < array.length; i++) {
+            var line = '';
+            for (var index in fields) {
+                    if (array[i][index].constructor == Array){
+                        var value = array[i][index][1] + "";
+                    }
+                    else{
+                        var value = array[i][index] + "";
+                    }
+                    line += '"' + value.replace(/"/g, '""') + '",';
+            }
+            line = line.slice(0, -1);
+            res += line + '\r\n';
+        }
+        //window.open("data:text/csv;charset=utf-8," + escape(res));
+        //var encodedUri = encodeURI(res);
+        //window.open(encodedUri);
+        var uri = 'data:text/csv;charset=utf-8,' + encodeURI(res);
+        var downloadLink = document.createElement("a");
+        downloadLink.href = uri;
+        downloadLink.download = title+'.csv';
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    }
 });
 
